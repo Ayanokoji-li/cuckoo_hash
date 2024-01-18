ee8ca29c8e3c717e512fd343021ea9704cb94f6e
Li <lixiang2022@shanghaitech.edu.cn>
2024-01-18 20:37:48 +0800

    modify

--- run.sh
+++ run.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
-# echo "---------------------"
-# echo "task 1 start"
+echo "---------------------"
+echo "task 1 start"
 
-# for i in $(seq 10 1 24)
-# do
-#     ./task1 --repeat 5 --insert $i
-# done
+for i in $(seq 10 1 24)
+do
+    ./task1 --repeat 5 --insert $i
+done
 
-# echo "task 1 end"
-# echo ""
-# echo "---------------------"
-# echo "task 2 start"
+echo "task 1 end"
+echo ""
+echo "---------------------"
+echo "task 2 start"
 
-# ./task2 --repeat 5
+./task2 --repeat 5
 
-# echo "task 2 end"
-# echo ""
-# echo "---------------------"
+echo "task 2 end"
+echo ""
+echo "---------------------"
 
-# echo "task 3 start"
+echo "task 3 start"
 
-# for i in $(seq 1.1 0.1 2)
-# do
-#     ./task3 --ratio $i --repeat 5
-# done
-# ./task3 --ratio 1.01 --repeat 5
-# ./task3 --ratio 1.02 --repeat 5
-# ./task3 --ratio 1.05 --repeat 5
+for i in $(seq 1.1 0.1 2)
+do
+    ./task3 --ratio $i --repeat 5
+done
+./task3 --ratio 1.01 --repeat 5
+./task3 --ratio 1.02 --repeat 5
+./task3 --ratio 1.05 --repeat 5
 
-# echo "task 3 end"
-# echo ""
-# echo "----------------------"
+echo "task 3 end"
+echo ""
+echo "----------------------"
 
 echo "task 4 start"
 for i in $(seq 1 1 10)
